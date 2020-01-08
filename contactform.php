979e3932e8c2d2b9e7c73560e6238c180f7ce74c
Bartosz Matusik <bartosz.matusik7@gmail.com>
2020-01-08 20:30:00 +0100

    complete style contact form, delete 'subject' php

--- contactform.php
+++ contactform.php
@@ -2,7 +2,7 @@
 
 if (isset($_POST['submit'])) {
 $name = $_POST['name'];
-$subject = $_POST['subject'];
+// $subject = $_POST['subject'];
 $mailFrom = $_POST['mail'];
 $message = $_POST['message'];
 
@@ -10,6 +10,10 @@ $mailTo = "kontakt@pizzeriaroberto.pl";
 $headers = "From: ".$mailFrom;
 $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-mail($mailTo, $subject, $txt, $headers);
+mail($mailTo, $txt, $headers);
 header("Location: index.html?mailsend");
 }
+
+// mail($mailTo, $subject, $txt, $headers);
+// header("Location: index.html?mailsend");
+// }
