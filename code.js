3b6e584cab3f5a97431bc735afcc707c82fdfdc5
Bartosz Matusik <bartosz.matusik7@gmail.com>
2020-01-11 17:19:26 +0100

    corect email validation, after send alert, refresh

--- code.js
+++ code.js
@@ -60,13 +60,254 @@ const changeSlide = () => {
 
 setInterval(changeSlide, time);
 
-const dotClick = dots.toString
+// const dotClick = dots.toString
 
 
-const dotChangeSlide = () => {
-    console.log(dotClick)
-}
+// const dotChangeSlide = () => {
+//     console.log(dotClick)
+// }
 
 
 
-dotClick.addEventListener('click', dotChangeSlide);
+// dotClick.addEventListener('click', dotChangeSlide);
+
+
+
+
+
+
+
+// contact form
+
+const formMail = document.querySelector('.contact-form-input-mail');
+const formBtn = document.querySelector('.formBtn');
+
+
+// document.querySelector('.contact-form-input-subject').textContent = "Odpoweidź na wiadomość z formularza ze strony";
+
+
+
+// console.log(window.location.href);
+
+
+window.addEventListener("load", () => {
+    if (window.location.href == "http://demo.pizzeriaroberto.pl/index.html?mailsend") {
+        alert("Twoja wiadomość została wysłana.");
+        window.location.href = "http://demo.pizzeriaroberto.pl";
+    }
+})
+
+// formBtn.addEventListener("mouseover", function () {
+//     console.log('działa')
+// })
+
+
+// const mailAlert = () => {
+//     // const formMail = document.querySelector('.contact-form-input-mail');
+//     let val = formMail.textContent.value
+//     // if (val.includes('@')) {
+//     //     return
+//     // } else {
+//     //     alert('Proszę podać poprawny adres e-mail');
+//     //     formBtn.disabled = true;
+//     // }
+
+
+//     if (val = val.includes('a')) {
+//         return
+//     } else {
+//         alert('Proszę podać poprawny adres e-mail');
+//         formBtn.disabled = true;
+//     }
+// }
+
+
+
+
+// formMail.addEventListener('input', mailAlert)
+
+
+
+// const validate = formMail.addEventListener('input', (e) => {
+//     console.log(e.target.value)
+
+// if (e.target.value.includes('a')) {
+//     return
+// } else {
+//     alert('Proszę podać poprawny adres e-mail');
+//     formBtn.disabled = true;
+// }
+
+
+
+// formMail.addEventListener('focusout', () => {
+//         if (e.target.value.includes('@')) {
+//             // document.form1.mail.focus();
+//             formBtn.disabled = false;
+//             return true;
+
+//         } else {
+//             // document.form1.mail.focus();
+//             // return false;
+//             alert('Proszę podać poprawny adres e-mail');
+//             // document.form1.mail.focus();
+//             // formMail.required = true;
+//             formBtn.disabled = true;
+//             return false;
+//         }
+//     }
+
+// )
+
+
+
+
+// })
+
+
+
+// const validate = formMail.addEventListener('focusout', (e) => {
+//         if (e.target.value.includes('@')) {
+//             // document.form1.mail.focus();
+//             // formBtn.disabled = false;
+//             // formBtn.setAttribute("abled", "abled")
+//             return true;
+
+
+//         } else {
+//             // document.form1.mail.focus();
+//             // return false;
+//             alert('Proszę podać poprawny adres e-mail');
+//             // document.form1.mail.focus();
+//             // formMail.required = true;
+//             // formBtn.disabled = true;
+//             // formBtn.setAttribute("disabled", "disabled")
+//             return false;
+//         }
+
+
+
+
+
+//     }
+
+// )
+
+// formBtn.addEventListener('click', () => {
+//     console.log('button')
+// })
+
+
+
+
+// formBtn.addEventListener('click', () => {
+
+//     formMail.addEventListener('input', (e) => {
+//             if (e.target.value.includes('@')) {
+
+//                 // return true;
+//                 return
+
+
+//             } else {
+
+//                 alert('Proszę podać poprawny adres e-mail');
+
+//                 // return false;
+//             }
+
+
+
+
+
+//         }
+
+//     )
+
+
+// })
+
+formBtn.addEventListener('click', () => {
+    if (formMail.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+        return
+    } else {
+        alert('Proszę podać poprawny adres e-mail');
+        // formBtn.disabled = true;
+        // formBtn.disabled = false;
+        event.preventDefault()
+    }
+})
+
+
+
+// formBtn.addEventListener('click', () => {
+//     if (formMail.value.includes('@')) {
+//         return
+//     } else {
+//         alert('Proszę podać poprawny adres e-mail');
+//         // formBtn.disabled = true;
+//         // formBtn.disabled = false;
+//         event.preventDefault()
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+// formBtn.addEventListener('click', function validateMyForm() {
+//     if (formMail.value.includes('@')) {
+//         alert("validation failed false");
+//         returnToPreviousPage();
+//         return false;
+//     }
+
+//     alert("validations passed");
+//     return true;
+// })
+
+
+
+
+
+// formBtn.addEventListener('click', () => {
+//     if (validate == true) {
+//         return
+//     } else {
+//         alert('Proszę podać poprawny adres e-mail');
+//     }
+// })
+
+
+
+
+// formMail.addEventListener('focusout', () => {
+//         if (curentInput.includes('@')) {
+//             return
+//         } else {
+//             alert('Proszę podać poprawny adres e-mail');
+//             formBtn.disabled = true;
+//         }
+//     }
+
+// )
+
+// function ValidateEmail(inputText) {
+//     var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+//     if (inputText.value.match(mailformat)) {
+//         alert("You have entered a valid email address!"); //The pop up alert for a valid email address
+//         document.form1.text1.focus();
+//         return true;
+//     } else {
+//         alert("You have entered an invalid email address!"); //The pop up alert for an invalid email address
+//         document.form1.text1.focus();
+//         return false;
+//     }
+// }
--- contactform.php
+++ contactform.php
@@ -1,18 +1,24 @@
 <?php
+ iconv_set_encoding("internal_encoding", "UTF-8");
 
 if (isset($_POST['submit'])) {
 $name = $_POST['name'];
-// $subject = $_POST['subject'];
+$subject = 'Wiadomość wysłana przez formularz';
 $mailFrom = $_POST['mail'];
 $message = $_POST['message'];
 
 $mailTo = "kontakt@pizzeriaroberto.pl";
 $headers = "From: ".$mailFrom;
-$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+$txt = "Wiadomość wysłana przez ".$name.".\n\n".$message;
 
-mail($mailTo, $txt, $headers);
+// mail($mailTo, $txt, $headers);
+// header("Location: index.html?mailsend");
+// }
+
+mail(utf8_decode($mailTo), utf8_decode($subject), utf8_decode($txt), utf8_decode($headers));
 header("Location: index.html?mailsend");
 }
+
 
 // mail($mailTo, $subject, $txt, $headers);
 // header("Location: index.html?mailsend");
