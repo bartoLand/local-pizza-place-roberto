e4c02fc4fc34373d966b3b7eaa59229052c296aa
Bartosz Matusik <bartosz.matusik7@gmail.com>
2020-01-13 13:07:47 +0100

    correct polish special signs

--- contactform.php
+++ contactform.php
@@ -1,5 +1,5 @@
 <?php
- iconv_set_encoding("internal_encoding", "UTF-8");
+//  iconv_set_encoding("internal_encoding", "UTF-8");
 
 if (isset($_POST['submit'])) {
 $name = $_POST['name'];
@@ -15,11 +15,11 @@ $txt = "Wiadomość wysłana przez ".$name.".\n\n".$message;
 // header("Location: index.html?mailsend");
 // }
 
-mail(utf8_decode($mailTo), utf8_decode($subject), utf8_decode($txt), utf8_decode($headers));
-header("Location: index.html?mailsend");
-}
-
-
-// mail($mailTo, $subject, $txt, $headers);
+// mail(utf8_decode($mailTo), utf8_decode($subject), utf8_decode($txt), utf8_decode($headers));
 // header("Location: index.html?mailsend");
 // }
+
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.html?mailsend");
+}
