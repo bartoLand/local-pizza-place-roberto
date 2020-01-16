d2eb093d51d4275037f0d548caac3ab8f11357da
Bartosz Matusik <bartosz.matusik7@gmail.com>
2020-01-16 10:11:05 +0100

    add clickable bouttons from navigation

--- code.js
+++ code.js
@@ -82,14 +82,6 @@ setInterval(changeSlide, time);
 const formMail = document.querySelector('.contact-form-input-mail');
 const formBtn = document.querySelector('.formBtn');
 
-
-// document.querySelector('.contact-form-input-subject').textContent = "Odpoweidź na wiadomość z formularza ze strony";
-
-
-
-// console.log(window.location.href);
-
-
 window.addEventListener("load", () => {
     if (window.location.href == "http://demo.pizzeriaroberto.pl/index.html?mailsend") {
         alert("Twoja wiadomość została wysłana.");
@@ -97,158 +89,119 @@ window.addEventListener("load", () => {
     }
 })
 
-// formBtn.addEventListener("mouseover", function () {
-//     console.log('działa')
-// })
 
-
-// const mailAlert = () => {
-//     // const formMail = document.querySelector('.contact-form-input-mail');
-//     let val = formMail.textContent.value
-//     // if (val.includes('@')) {
-//     //     return
-//     // } else {
-//     //     alert('Proszę podać poprawny adres e-mail');
-//     //     formBtn.disabled = true;
-//     // }
-
-
-//     if (val = val.includes('a')) {
-//         return
-//     } else {
-//         alert('Proszę podać poprawny adres e-mail');
-//         formBtn.disabled = true;
-//     }
-// }
-
-
-
-
-// formMail.addEventListener('input', mailAlert)
-
-
-
-// const validate = formMail.addEventListener('input', (e) => {
-//     console.log(e.target.value)
-
-// if (e.target.value.includes('a')) {
-//     return
-// } else {
-//     alert('Proszę podać poprawny adres e-mail');
-//     formBtn.disabled = true;
-// }
-
-
-
-// formMail.addEventListener('focusout', () => {
-//         if (e.target.value.includes('@')) {
-//             // document.form1.mail.focus();
-//             formBtn.disabled = false;
-//             return true;
-
-//         } else {
-//             // document.form1.mail.focus();
-//             // return false;
-//             alert('Proszę podać poprawny adres e-mail');
-//             // document.form1.mail.focus();
-//             // formMail.required = true;
-//             formBtn.disabled = true;
-//             return false;
-//         }
-//     }
-
-// )
-
-
-
-
-// })
-
-
-
-// const validate = formMail.addEventListener('focusout', (e) => {
-//         if (e.target.value.includes('@')) {
-//             // document.form1.mail.focus();
-//             // formBtn.disabled = false;
-//             // formBtn.setAttribute("abled", "abled")
-//             return true;
-
-
-//         } else {
-//             // document.form1.mail.focus();
-//             // return false;
-//             alert('Proszę podać poprawny adres e-mail');
-//             // document.form1.mail.focus();
-//             // formMail.required = true;
-//             // formBtn.disabled = true;
-//             // formBtn.setAttribute("disabled", "disabled")
-//             return false;
-//         }
-
-
-
-
-
-//     }
-
-// )
-
-// formBtn.addEventListener('click', () => {
-//     console.log('button')
-// })
-
-
-
-
-// formBtn.addEventListener('click', () => {
-
-//     formMail.addEventListener('input', (e) => {
-//             if (e.target.value.includes('@')) {
-
-//                 // return true;
-//                 return
-
-
-//             } else {
-
-//                 alert('Proszę podać poprawny adres e-mail');
-
-//                 // return false;
-//             }
-
-
-
-
-
-//         }
-
-//     )
-
-
-// })
 
 formBtn.addEventListener('click', () => {
     if (formMail.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
         return
     } else {
         alert('Proszę podać poprawny adres e-mail');
-        // formBtn.disabled = true;
-        // formBtn.disabled = false;
         event.preventDefault()
     }
 })
 
 
 
-// formBtn.addEventListener('click', () => {
-//     if (formMail.value.includes('@')) {
-//         return
-//     } else {
-//         alert('Proszę podać poprawny adres e-mail');
-//         // formBtn.disabled = true;
-//         // formBtn.disabled = false;
-//         event.preventDefault()
-//     }
+
+
+
+
+
+
+// scroll to by click on buttons
+
+
+const pizzeriaBtn = document.querySelector('.pizzeriaBtn');
+const menuBtn = document.querySelector('.menuBtn');
+const galleryBtn = document.querySelector('.galleryBtn');
+const contactBtn = document.querySelector('.contactBtn');
+const reservationBtn = document.querySelector('.reservationBtn');
+
+const pizzeriaBtn2 = document.querySelector('.pizzeriaBtn2');
+const menuBtn2 = document.querySelector('.menuBtn2');
+const galleryBtn2 = document.querySelector('.galleryBtn2');
+const contactBtn2 = document.querySelector('.contactBtn2');
+const reservationBtn2 = document.querySelector('.reservationBtn2');
+
+// const pizzeriaBtn3 = document.querySelector('.pizzeriaBtn3');
+// const menuBtn3 = document.querySelector('.menuBtn3');
+// const galleryBtn3 = document.querySelector('.galleryBtn3');
+// const contactBtn3 = document.querySelector('.contactBtn3');
+// const reservationBtn3 = document.querySelector('.reservationBtn3');
+
+const menuBannerBtn = document.querySelector('.banner-menu ');
+
+// position of sections
+
+const offsetPizzeria = document.querySelector('.pizzeria').offsetTop;
+const offsetMenu = document.querySelector(".section-menu").offsetTop;
+const offsetGallery = document.querySelector(".section-gallery").offsetTop;
+const offsetContact = document.querySelector(".contact").offsetTop;
+const offsetReservation = document.querySelector(".contact-form").offsetTop;
+
+
+// go to specific section
+
+const goToPizzeria = () => {
+    window.scrollTo(0, offsetPizzeria - 130)
+}
+
+const goToMenu = () => {
+    window.scrollTo(0, offsetMenu - 130);
+}
+
+const goToGallery = () => {
+    window.scrollTo(0, offsetGallery - 130).animateScroll(target);
+
+}
+
+const goToContact = () => {
+    window.scrollTo(0, offsetContact - 130)
+}
+
+const goToReservation = () => {
+    window.scrollTo(0, offsetReservation - 130)
+}
+
+
+
+
+
+//functions on every menu button
+
+pizzeriaBtn.addEventListener('click', goToPizzeria);
+menuBtn.addEventListener('click', goToMenu);
+galleryBtn.addEventListener('click', goToGallery);
+contactBtn.addEventListener('click', goToContact);
+reservationBtn.addEventListener('click', goToReservation);
+
+pizzeriaBtn2.addEventListener('click', goToPizzeria);
+menuBtn2.addEventListener('click', goToMenu);
+galleryBtn2.addEventListener('click', goToGallery);
+contactBtn2.addEventListener('click', goToContact);
+reservationBtn2.addEventListener('click', goToReservation);
+
+// pizzeriaBtn3.addEventListener('click', goToPizzeria);
+// menuBtn3.addEventListener('click', goToMenu);
+// galleryBtn3.addEventListener('click', goToGallery);
+// contactBtn3.addEventListener('click', goToContact);
+// reservationBtn3.addEventListener('click', goToReservation);
+
+menuBannerBtn.addEventListener('click', goToMenu);
+
+
+// menuBannerBtn.addEventListener('click', console.log('działa btn'))
+
+
+
+
+
+
+
+
+
+// window.addEventListener("scroll", () => {
+//     console.log(window.pageYOffset)
 // })
 
 
@@ -258,56 +211,4 @@ formBtn.addEventListener('click', () => {
 
 
 
-
-
-
-
-// formBtn.addEventListener('click', function validateMyForm() {
-//     if (formMail.value.includes('@')) {
-//         alert("validation failed false");
-//         returnToPreviousPage();
-//         return false;
-//     }
-
-//     alert("validations passed");
-//     return true;
-// })
-
-
-
-
-
-// formBtn.addEventListener('click', () => {
-//     if (validate == true) {
-//         return
-//     } else {
-//         alert('Proszę podać poprawny adres e-mail');
-//     }
-// })
-
-
-
-
-// formMail.addEventListener('focusout', () => {
-//         if (curentInput.includes('@')) {
-//             return
-//         } else {
-//             alert('Proszę podać poprawny adres e-mail');
-//             formBtn.disabled = true;
-//         }
-//     }
-
-// )
-
-// function ValidateEmail(inputText) {
-//     var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
-//     if (inputText.value.match(mailformat)) {
-//         alert("You have entered a valid email address!"); //The pop up alert for a valid email address
-//         document.form1.text1.focus();
-//         return true;
-//     } else {
-//         alert("You have entered an invalid email address!"); //The pop up alert for an invalid email address
-//         document.form1.text1.focus();
-//         return false;
-//     }
-// }
+// src = "https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15/dist/smooth-scroll.polyfills.min.js"
