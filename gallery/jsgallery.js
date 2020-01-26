8c82d048be6bcc5b062c3cb9268a7c27e55397a1
Bartosz Matusik <bartosz.matusik7@gmail.com>
2020-01-26 15:43:31 +0100

    add function to buttons

--- gallery/jsgallery.js
+++ gallery/jsgallery.js
@@ -15,3 +15,94 @@ window.addEventListener("scroll", function () {
         nav.style.top = "-100px";
     }
 })
+
+
+
+// scroll to by click on buttons
+
+
+// const pizzeriaBtn = document.querySelector('.pizzeriaBtn');
+// const menuBtn = document.querySelector('.menuBtn');
+// const galleryBtn = document.querySelector('.galleryBtn');
+// const contactBtn = document.querySelector('.contactBtn');
+// const reservationBtn = document.querySelector('.reservationBtn');
+
+// const pizzeriaBtn2 = document.querySelector('.pizzeriaBtn2');
+// const menuBtn2 = document.querySelector('.menuBtn2');
+// const galleryBtn2 = document.querySelector('.galleryBtn2');
+// const contactBtn2 = document.querySelector('.contactBtn2');
+// const reservationBtn2 = document.querySelector('.reservationBtn2');
+
+// const pizzeriaBtn3 = document.querySelector('.pizzeriaBtn3');
+// const menuBtn3 = document.querySelector('.menuBtn3');
+// const galleryBtn3 = document.querySelector('.galleryBtn3');
+// const contactBtn3 = document.querySelector('.contactBtn3');
+// const reservationBtn3 = document.querySelector('.reservationBtn3');
+
+// const menuBannerBtn = document.querySelector('.banner-menu ');
+
+// position of sections
+
+// const offsetPizzeria = document.querySelector('.pizzeria').offsetTop;
+// const offsetMenu = document.querySelector(".section-menu").offsetTop;
+// const offsetGallery = document.querySelector(".section-gallery").offsetTop;
+// const offsetContact = document.querySelector(".contact").offsetTop;
+// const offsetReservation = document.querySelector(".contact-form").offsetTop;
+
+
+// go to specific section
+
+// const goToPizzeria = () => {
+//     location.replace("https://www.w3schools.com")
+//     window.scrollTo(0, offsetPizzeria - 130)
+// }
+
+// const goToMenu = () => {
+//     window.scrollTo(0, offsetMenu - 130);
+// }
+
+// const goToGallery = () => {
+//     window.scrollTo(0, offsetGallery - 130).animateScroll(target);
+
+// }
+
+// const goToContact = () => {
+//     window.scrollTo(0, offsetContact - 130)
+// }
+
+// const goToReservation = () => {
+//     window.scrollTo(0, offsetReservation - 130)
+// }
+
+
+
+
+
+//functions on every menu button
+
+// pizzeriaBtn.addEventListener('click', goToPizzeria);
+// menuBtn.addEventListener('click', goToMenu);
+// galleryBtn.addEventListener('click', goToGallery);
+// contactBtn.addEventListener('click', goToContact);
+// reservationBtn.addEventListener('click', goToReservation);
+
+// pizzeriaBtn2.addEventListener('click', goToPizzeria);
+// menuBtn2.addEventListener('click', goToMenu);
+// galleryBtn2.addEventListener('click', goToGallery);
+// contactBtn2.addEventListener('click', goToContact);
+// reservationBtn2.addEventListener('click', goToReservation);
+
+// pizzeriaBtn3.addEventListener('click', goToPizzeria);
+// menuBtn3.addEventListener('click', goToMenu);
+// galleryBtn3.addEventListener('click', goToGallery);
+// contactBtn3.addEventListener('click', goToContact);
+// reservationBtn3.addEventListener('click', goToReservation);
+
+// menuBannerBtn.addEventListener('click', goToMenu);
+
+
+
+function goToMainPage() {
+    location.href = "/index.html";
+
+}
