f83a5e0dd5db7b79664e65c2bc3fad496d6523d1
Bartosz Matusik <bartosz.matusik7@gmail.com>
2020-01-26 16:29:54 +0100

    add link from main page to gallery

--- code.js
+++ code.js
@@ -19,46 +19,46 @@ window.addEventListener("scroll", function () {
 
 // slider gallery section
 
-const slideList = [{
-    img: "images/slider1.jpg",
-    text: "pierwszy tekst",
-}, {
-    img: "images/slider2.jpg",
-    text: "drugi tekst",
-}, {
-    img: "images/slider3.jpg",
-    text: "trzeci tekst",
-}]
+// const slideList = [{
+//     img: "images/slider1.jpg",
+//     text: "pierwszy tekst",
+// }, {
+//     img: "images/slider2.jpg",
+//     text: "drugi tekst",
+// }, {
+//     img: "images/slider3.jpg",
+//     text: "trzeci tekst",
+// }]
 
-// interface for gallery
+// // interface for gallery
 
-const image = document.querySelector("img.slider")
-const sliderText = document.querySelector("p.slider-text");
+// const image = document.querySelector("img.slider")
+// const sliderText = document.querySelector("p.slider-text");
 
-const dots = [...document.querySelectorAll(".dots span")];
-
-
-const time = 3000;
-let active = 0;
-
-const changeDot = () => {
-    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
-    dots[activeDot].classList.remove("active");
-    dots[active].classList.add("active");
-}
-
-const changeSlide = () => {
-    active++;
-    if (active === slideList.length) {
-        active = 0;
-    }
-    image.src = slideList[active].img;
-    sliderText.textContent = slideList[active].text;
-    changeDot();
-}
+// const dots = [...document.querySelectorAll(".dots span")];
 
 
-setInterval(changeSlide, time);
+// const time = 3000;
+// let active = 0;
+
+// const changeDot = () => {
+//     const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+//     dots[activeDot].classList.remove("active");
+//     dots[active].classList.add("active");
+// }
+
+// const changeSlide = () => {
+//     active++;
+//     if (active === slideList.length) {
+//         active = 0;
+//     }
+//     image.src = slideList[active].img;
+//     sliderText.textContent = slideList[active].text;
+//     changeDot();
+// }
+
+
+// setInterval(changeSlide, time);
 
 // const dotClick = dots.toString
 
@@ -212,3 +212,15 @@ menuBannerBtn.addEventListener('click', goToMenu);
 
 
 // src = "https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15/dist/smooth-scroll.polyfills.min.js"
+
+
+
+
+// const goToGallery = () => {
+//     location.href = "https://www.netflix.com/title/80197526";
+// }
+
+function goToGalleryLink() {
+    location.href = "gallery/gallery.html";
+
+}
